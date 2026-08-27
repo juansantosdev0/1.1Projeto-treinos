e5622416c6ccc60541e0d296a71dfc3753a40c85
Juan.Santos <juan.figueiredo@ba.estudante.senai.br>
2026-08-27 14:49:53 -0300

    Add files via upload

---
+++ Conversor simpes.py
@@ -0,0 +1,7 @@
+import os
+os.system ("clS")
+
+Conversor = float(input("Digite o valor em metros: "))
+Conversor = Conversor * 100
+print("O valor em centímetros é: ", Conversor)
+print("Fim do programa")
